3e071b35c3ecdc06a57a116cf87128bd2f410a03
Ben Griffis <griffis.ben16@gmail.com>
2022-12-04 21:21:40 -0500

    Add UZ/KZ

--- ws_sort_st.py
+++ ws_sort_st.py
@@ -17,7 +17,7 @@ matplotlib.rcParams.update(matplotlib.rcParamsDefault)
 
 st.title('Soccer Prospect Research & Radar Creation')
 st.subheader("All data from Wyscout (please see footer for each league's latest data update)")
-st.subheader('Created by Ben Griffis (Twitter: @BeGriffis | www.cafetactiques.com)')
+st.subheader('Created by Ben Griffis (Twitter: @BeGriffis)')
 
 with st.expander('Read App Details'):
     st.write('''
@@ -42,8 +42,8 @@ with st.sidebar:
     league = st.selectbox('League', ('K League 1', 'K League 2', 'J1', 'J2', 'J3', 'Chinese Super League',
                                      'Indonesian Liga 1', 'Thai League 1', 'Malaysian Super League',
                                      'Latvian Virsliga', 'Estonian Meistriliiga', 'Allsvenskan',
-                                     'Eliteserien', 'Veikkausliiga', 'MLS', 'Uruguayan Primera División',
-                                    'Chilean Primera División', 'Brasileirão', 'Argentinian Primera División'))
+                                     'Eliteserien', 'Veikkausliiga', 'MLS', 'Argentinian Primera División', 'Chilean Primera División',
+                                    'Uruguayan Primera División', 'Brasileirão', 'Uzbek Super League', 'Kazakh Premier League'))
     pos = st.selectbox('Positions', ('Strikers', 'Strikers and Wingers', 'Forwards (AM, W, CF)',
                                     'Forwards no ST (AM, W)', 'Wingers', 'Central Midfielders (DM, CM, CAM)',
                                     'Central Midfielders no CAM (DM, CM)', 'Fullbacks (FBs/WBs)',
@@ -1236,9 +1236,9 @@ try:
     #######################################################################################################
     #######################################################################################################
     #######################################################################################################
-    complete = ['Allsvenskan', 'Eliteserien', 'Estonian Meistriliiga', 'J1', 'J2', 'J3', 'K League 1', 'K League 2', 'Latvian Virsliga', 'Malaysian Super League', 'Veikkausliiga', 'MLS', 'Uruguayan Primera División', 'Chilean Primera División', 'Brasileirão', 'Argentinian Primera División']
+    complete = ['Allsvenskan', 'Eliteserien', 'Estonian Meistriliiga', 'J1', 'J2', 'J3', 'K League 1', 'K League 2', 'Latvian Virsliga', 'Malaysian Super League', 'Veikkausliiga', 'MLS', 'Uruguayan Primera División', 'Chilean Primera División', 'Brasileirão', 'Argentinian Primera División', 'Uzbek Super League']
     incomplete = ['Chinese Super League', 'Indonesian Liga 1', 'Thai League 1']
-    summer = ['Allsvenskan', 'Eliteserien', 'Estonian Meistriliiga', 'J1', 'J2', 'J3', 'K League 1', 'K League 2', 'Latvian Virsliga', 'Malaysian Super League', 'Veikkausliiga', 'Chinese Super League', 'MLS', 'Uruguayan Primera División', 'Chilean Primera División', 'Brasileirão', 'Argentinian Primera División']
+    summer = ['Allsvenskan', 'Eliteserien', 'Estonian Meistriliiga', 'J1', 'J2', 'J3', 'K League 1', 'K League 2', 'Latvian Virsliga', 'Malaysian Super League', 'Veikkausliiga', 'Chinese Super League', 'MLS', 'Uruguayan Primera División', 'Chilean Primera División', 'Brasileirão', 'Argentinian Primera División', 'Kazakh Premier League']
     winter = ['Indonesian Liga 1', 'Thai League 1']
     
     if league in complete:
@@ -1307,11 +1307,13 @@ with st.expander('Latest Data Updates'):
     Indonesian Liga 1: After league stoppage in October  \n
     J1, J2, J3: DATA FINAL FOR 2022  \n
     K League 1 & 2: DATA FINAL FOR 2022  \n
+    Kazakh Premier League: DATA FINAL FOR 2022  \n
     Latvian Virsliga: DATA FINAL FOR 2022  \n
     Malaysian Super League: DATA FINAL FOR 2022  \n
     MLS: DATA FINAL FOR 2022  \n
     Thai League 1: 12/4/22  \n
     Uruguayan Primera División: DATA FINAL FOR 2022  \n
+    Uzbek Super League: DATA FINAL FOR 2022  \n
     Veikkausliiga: DATA FINAL FOR 2022
     ''')
     
