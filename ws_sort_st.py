cfbcb666c5884957b7de5effa1901f3685033713
Ben Griffis <griffis.ben16@gmail.com>
2023-01-12 16:46:19 -0500

    Update slider minutes to int input

--- ws_sort_st.py
+++ ws_sort_st.py
@@ -47,7 +47,7 @@ with st.sidebar:
                                     'Forwards no ST (AM, W)', 'Wingers', 'Central Midfielders (DM, CM, CAM)',
                                     'Central Midfielders no CAM (DM, CM)', 'Fullbacks (FBs/WBs)',
                                     'Defenders (CB, FB/WB, DM)', 'Centre-Backs'))
-    mins = st.slider('Minimum Minutes Played', 300, max(df['Minutes played'].astype(int)), 900)
+    mins = st.number_input('Minimum Minutes Played', 300, max(df['Minutes played'].astype(int)), 900)
     maxage = st.slider('Max Age', 15, max(df.Age.astype(int)), 25)
 
 
