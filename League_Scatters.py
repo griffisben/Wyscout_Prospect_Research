84d494fa5b4fd297c220f1790cd21418a1e6d69a
Ben Griffis <griffis.ben16@gmail.com>
2023-02-27 15:49:41 -0500

    Update League_Scatters.py

--- League_Scatters.py
+++ League_Scatters.py
@@ -132,6 +132,7 @@ fig = px.scatter(
     dfProspect,
     x = xx,
     y = yy,
+    color = 'Team'
 #     marginal_x='rug',
 #     marginal_y='rug',
     text = 'Player',
@@ -140,7 +141,7 @@ fig = px.scatter(
     title = '%s, %s & %s <br><sup>%s | Minimum %i minutes played | Code by @BeGriffis</sup>' %(league,xx,yy,pos,mins),
     width=900,
     height=700)
-fig.update_traces(textposition='top right', marker_size=10, marker_color='#4A2E19')
+fig.update_traces(textposition='top right', marker_size=10)
 
 fig.add_hline(y=dfProspect[yy].median(), name='Median', line_width=0.5)
 fig.add_vline(x=dfProspect[xx].median(), name='Median', line_width=0.5)
