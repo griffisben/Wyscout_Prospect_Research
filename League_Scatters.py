0d70a0ef848e081979b428836a3a4e0e3dd84fe5
Ben Griffis <griffis.ben16@gmail.com>
2023-10-16 11:49:13 -0400

    Update League_Scatters.py

--- League_Scatters.py
+++ League_Scatters.py
@@ -73,8 +73,7 @@ with st.sidebar:
     if st.session_state.clicked:
         xx = flipY
         yy = flipX
-        if 'clicked' in st.session_state:
-            st.session_state.clicked = False
+        st.session_state.clicked = False
         
 
 ssn = lg_lookup[lg_lookup['League']==league].Season.values[0]
