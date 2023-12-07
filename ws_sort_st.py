3f0399468d53a013dbb4b9abe0322a6efed6a241
Ben Griffis <griffis.ben16@gmail.com>
2023-12-07 10:09:55 -0500

    Update ws_sort_st.py

--- ws_sort_st.py
+++ ws_sort_st.py
@@ -930,7 +930,7 @@ df1 = df1.dropna(subset=['Position', 'Team within selected timeframe', 'Age']).r
 df1 = df1.dropna(subset=['Position']).reset_index(drop=True)
 df1['Age'] = df1['Age'].astype(int)
 df1['Main Position'] = df1['Position'].str.split().str[0].str.rstrip(',')
-df1 = df1.dropna(subset=['Main Position']).reset_index(drop=True)
+df1 = df1.dropna(subset=['Main Position'])
 df1['Main Position'] = df1['Main Position'].replace('LAMF','LW')
 df1['Main Position'] = df1['Main Position'].replace('RAMF','RW')
 df1['Main Position'] = df1['Main Position'].replace('LCB3','LCB')
