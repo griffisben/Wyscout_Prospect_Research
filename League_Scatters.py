69d91ec112838b639b131fa494337c45d6fb7f15
Ben Griffis <griffis.ben16@gmail.com>
2024-02-08 15:53:08 -0500

    Update League_Scatters.py

--- League_Scatters.py
+++ League_Scatters.py
@@ -66,7 +66,11 @@ df['Main Position'] = df['Main Position'].replace('RAMF','RW')
 
 with st.sidebar:
     st.header('Choose Basic Options')
-    lg_season = st.selectbox('Season', (['2023','23-24']))
+    with st.expander('Note on Seasons'):
+        st.write('''
+        Please note that with prior seasons, the players & leagues are correct but the team names can sometimes be off. Ages are also current ages, not ages in the season... I'm working on remedying this.
+        ''')
+    lg_season = st.selectbox('Season', (['23-24','2023','22-23','2022','21-22']))
     lg_lookup_ssn = lg_lookup[lg_lookup.Season==lg_season]
     league = st.selectbox('League', (lg_lookup_ssn.League.tolist()))
     pos = st.selectbox('Positions', ('Strikers', 'Strikers and Wingers', 'Forwards (AM, W, CF)',
