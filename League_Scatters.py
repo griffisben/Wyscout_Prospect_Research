82fbf36416d42b221dddd61659c50a8eb02a1bbd
Ben Griffis <griffis.ben16@gmail.com>
2026-06-21 09:39:07 +0200

    Update League_Scatters.py

--- League_Scatters.py
+++ League_Scatters.py
@@ -1,1 +1,3 @@
+import streamlit as st
+
 st.write("This app is no longer supported... please visit https://best11scouting.streamlit.app/ which is much more comprehensive and includes the scatter tool as a feature. Thank you!")
